9947d6fb104a4cf89be2fb0a790c5c4f2f9811ff
timothyphilip <philippetimothe@gmail.com>
2020-11-08 12:39:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+  }, false);
+  
 });
+
+// document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
